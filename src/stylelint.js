cc4fd9ccafef456e43d82c2ae7dfd01344d65c3e
qhbhq <qiuhongbingo@outlook.com>
2021-02-02 21:50:13 +0800

    feat(stylelint): use extends & add rules

--- src/stylelint.js
+++ src/stylelint.js
@@ -1,1 +1,14 @@
-// TODO
+module.exports = {
+  extends: ['stylelint-config-prettier', 'stylelint-config-standard'],
+
+  rules: {
+    'rule-empty-line-before': [
+      'always',
+      { except: ['after-single-line-comment', 'first-nested'] },
+    ],
+    'color-named': 'never',
+    'color-hex-case': 'lower',
+    'color-hex-length': 'short',
+    'no-descending-specificity': null,
+  },
+}
