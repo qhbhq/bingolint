be75c03c5a8c507cb68522638ba56e754ea859a5
qhbhq <qiuhongbingo@outlook.com>
2021-02-25 17:00:52 +0800

    feat: move main

---
+++ index.js
@@ -0,0 +1,9 @@
+const eslint = require('./src/eslint')
+const prettier = require('./src/prettier')
+const stylelint = require('./src/stylelint')
+
+module.exports = {
+  eslint,
+  prettier,
+  stylelint,
+}
