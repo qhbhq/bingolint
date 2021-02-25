9b3d42fe770c8bc44f6c0e3007083145d083d4c6
qhbhq <qiuhongbingo@outlook.com>
2021-02-25 16:36:28 +0800

    docs: add stylelint & prettier rule comment

--- src/prettier.js
+++ src/prettier.js
@@ -1,21 +1,28 @@
 module.exports = {
+  // Only add semicolons at the beginning of lines that may introduce ASI failures
   semi: false,
+
+  // Use single quotes instead of double quotes
   singleQuote: true,
+
+  // Trailing commas wherever possible
   trailingComma: 'all',
+
+  // Do not wrap prose
   proseWrap: 'never',
+
+  // Line Feed only (\n), common on Linux and macOS as well as inside git repos
   endOfLine: 'lf',
+
+  // Configuration Overrides
   overrides: [
     {
       files: '.prettierrc',
-      options: {
-        parser: 'json',
-      },
+      options: { parser: 'json' },
     },
     {
       files: 'document.ejs',
-      options: {
-        parser: 'html',
-      },
+      options: { parser: 'html' },
     },
   ],
 }
--- src/stylelint.js
+++ src/stylelint.js
@@ -7,19 +7,28 @@ module.exports = {
 
   plugins: ['stylelint-order'],
 
+  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+
   rules: {
+    // Require an empty line before rules
     'rule-empty-line-before': [
       'always',
-      {
-        except: ['after-single-line-comment', 'first-nested'],
-      },
+      { except: ['after-single-line-comment', 'first-nested'] },
     ],
+
+    // Colors must never be named
     'color-named': 'never',
+
+    // Specify lowercase for hex colors
     'color-hex-case': 'lower',
+
+    // Specify short notation for hex colors
     'color-hex-length': 'short',
+
+    // Urls must always be quoted
     'function-url-quotes': 'always',
+
+    // Allow selectors of lower specificity from coming after overriding selectors of higher specificity
     'no-descending-specificity': null,
   },
-
-  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
 }
